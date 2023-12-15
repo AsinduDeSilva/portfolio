400e10647af080a46bdd10de4ad59318f8ae80a2
Asindu De Silva <asindudesilva@gmail.com>
2023-12-15 08:16:20 +0530

    Configured MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Navbar from "./components/Navbar"
 
 function App() {
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#fff',
+      }, 
+      secondary: {
+        main: '#6649be',
+      },
+    },
+  });
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Navbar/>
-    </>
-      
+    </ThemeProvider>
   )
 }
 
